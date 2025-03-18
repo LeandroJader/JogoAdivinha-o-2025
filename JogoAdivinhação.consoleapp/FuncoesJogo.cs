f91bc42682030dea18ff84ff7b9ae024b669e5af
leandroJader <ldandro>
2025-03-18 10:53:01 -0300

    ''

--- JogoAdivinhação.consoleapp/FuncoesJogo.cs
+++ JogoAdivinhação.consoleapp/FuncoesJogo.cs
@@ -1,75 +1,62 @@
 namespace JogoAdivinhação.ConsoleApp
 {
-    internal class Program
+    public class FuncoesJogo
     {
-        static void Main(string[] args)
+
+        public static int GerarNumerosAleatorios()
         {
-            while (true)
+            Random geradorDeNumeros = new Random();
+            return geradorDeNumeros.Next(1, 21);
+        }
+
+
+        public void LoopTentativas(int totalDeTentativas, int numeroSecreto)
+        {
+            for (int tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
             {
-                ExibirMenu();
+                Console.Clear();
+                Console.WriteLine("========================================");
+                Console.WriteLine($"Tentativa {tentativa} de {totalDeTentativas}");
+                Console.WriteLine("========================================");
 
-                string opcaoDificuldade = EscolherDificuldade();
-                int totalDeTentativas = 0;
+                // Lógica do jogo
+                Console.Write("Digite um número entre 1 e 20: ");
+                int numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
-                if (DificuldadeFacilEscolhida(opcaoDificuldade))
-                    totalDeTentativas = 10;
-                else if (DificuldadeNormalEscolhida(opcaoDificuldade))
-                    totalDeTentativas = 5;
-                else if (DificuldadeDificilEscolhida(opcaoDificuldade))
-                    totalDeTentativas = 3;
-
-
-                int numeroSecreto = FuncoesJogo.GerarNumerosAleatorios();
-
-
-                FuncoesJogo jogo = new FuncoesJogo();
-                jogo.LoopTentativas(totalDeTentativas, numeroSecreto);
-
-
-                Console.Write("Deseja continuar (S/N)? ");
-                string opcaoContinuar = Console.ReadLine().ToUpper();
-
-                if (opcaoContinuar != "S")
+                if (numeroDigitado == numeroSecreto)
+                {
+                    Console.WriteLine("========================================");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Parabéns! Você acertou!");
+                    Console.ResetColor();
+                    Console.WriteLine("========================================");
                     break;
+                }
+                else if (tentativa == totalDeTentativas)
+                {
+                    Console.WriteLine("========================================");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Que pena! Você usou todas as tentativas. O número era {numeroSecreto}.");
+                    Console.ResetColor();
+                    Console.WriteLine("========================================");
+                    break;
+                }
+                else if (numeroDigitado > numeroSecreto)
+                {
+                    Console.WriteLine("========================================");
+                    Console.WriteLine("O número secreto é menor que o número digitado.");
+                    Console.WriteLine("========================================");
+                }
+                else
+                {
+                    Console.WriteLine("========================================");
+                    Console.WriteLine("O número secreto é maior que o número digitado.");
+                    Console.WriteLine("========================================");
+                }
+
+                Console.WriteLine("Aperte ENTER para continuar...");
+                Console.ReadLine();
             }
-        }
-
-        static void ExibirMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("========================================");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Jogo de Adivinhação");
-            Console.ResetColor();
-            Console.WriteLine("========================================");
-        }
-
-        static string EscolherDificuldade()
-        {
-            Console.WriteLine("Escolha um nível de dificuldade:");
-            Console.WriteLine("========================================");
-            Console.WriteLine("1 - Fácil (10 tentativas)");
-            Console.WriteLine("2 - Normal (5 tentativas)");
-            Console.WriteLine("3 - Difícil (3 tentativas)");
-            Console.WriteLine("========================================");
-            Console.Write("Digite sua escolha: ");
-            string opcaoDificuldade = Console.ReadLine();
-            return opcaoDificuldade;
-        }
-
-        static bool DificuldadeFacilEscolhida(string dificuldade)
-        {
-            return dificuldade == "1";
-        }
-
-        static bool DificuldadeNormalEscolhida(string dificuldade)
-        {
-            return dificuldade == "2";
-        }
-
-        static bool DificuldadeDificilEscolhida(string dificuldade)
-        {
-            return dificuldade == "3";
         }
     }
 }
